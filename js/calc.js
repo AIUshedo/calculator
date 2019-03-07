39f209795a68786d1de8101318b1024acba81f3c
unknown <aushedo@spartaglobal.com>
2019-03-07 11:04:31 +0000

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+var button = document.getElementsByTagName("button");
+var symbols = document.getElementsByClassName("operator");
+
+var clear = document.getElementsByClassName("buttonClear");
+clear.addEventListener("click", )
+
+var finalanswer = document.getElementsByClassName("equals");
+var spartaCal = document.getElementById("screen");
+//
+function getFirstNumber() {
+  
+}
+
+});
